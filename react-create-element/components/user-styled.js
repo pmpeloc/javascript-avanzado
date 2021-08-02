0df5a5a6bc4c379b9f9215a8ddc7fc8871296da6
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-08-02 17:35:14 -0300

    interpolacion theme dark/light

--- react-create-element/components/user-styled.js
+++ react-create-element/components/user-styled.js
@@ -5,13 +5,14 @@ import { createElement } from '../lib/react/index.js';
 const UserStyled = styled.div`
   background-image: linear-gradient(
     to bottom,
-    #f9f9f9 0%,
-    #f9f9f9 130px,
-    rgba(0, 0, 0, 0.15) 130px,
-    rgba(0, 0, 0, 0.15) 131px,
-    white 131px,
-    white 100%
+    ${({ primaryColor }) => primaryColor} 0%,
+    ${({ primaryColor }) => primaryColor} 130px,
+    ${({ tertiaryColor }) => tertiaryColor} 130px,
+    ${({ tertiaryColor }) => tertiaryColor} 131px,
+    ${({ secondaryColor }) => secondaryColor} 131px,
+    ${({ secondaryColor }) => secondaryColor} 100%
   );
+  color: ${({ fontColor }) => fontColor};
   text-align: center;
   overflow: hidden;
   padding: 20px;
@@ -71,11 +72,12 @@ class User extends Component {
     const { name, avatar } = this.props;
     return UserStyled(
       {
+        ...colors,
         children: [
           AvatarStyled({
             src: avatar,
           }),
-          createElement('h2', null, name),
+          createElement('h2', null, `${name} ${mode} mode`),
         ],
       },
       ''
--- react-create-element/lib/styled-components.js
+++ react-create-element/lib/styled-components.js
@@ -4,14 +4,24 @@ const styled = {};
 
 const elements = ['h1', 'p', 'div', 'img', 'article', 'footer', 'header'];
 
+function buildStyles(strings, dinamicValues, props) {
+  let style = strings.slice();
+  dinamicValues.forEach((value, index) => {
+    style[index] += value(props);
+  });
+  console.log(style);
+  return style.join('');
+}
+
 elements.forEach((tag) => {
-  styled[tag] = function (styles) {
+  styled[tag] = function (strings, ...dynamicValues) {
     return function (props, content) {
+      const style = buildStyles(strings, dynamicValues, props);
       return createElement(
         tag,
         {
           ...props,
-          style: styles,
+          style,
         },
         content
       );
