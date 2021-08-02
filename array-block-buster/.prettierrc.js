0279d7c8aefd85b4f21fdf5e83f3c2fb112931f7
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-08-02 12:15:51 -0300

    react create element

--- array-block-buster/.prettierrc.js
+++ array-block-buster/.prettierrc.js
@@ -2,5 +2,5 @@ module.exports = {
   trailingComma: "es5",
   tabWidth: 2,
   semi: true,
-  singleCuote: true,
+  singleQuote: true,
 };
---
+++ react-create-element/components/app.js
@@ -0,0 +1,40 @@
+import { Component, createElement } from '../lib/react/index.js';
+// import User from './user.js';
+// import Wrapper from './wrapper.js';
+// import UserStyled from './user-styled.js';
+
+const element = createElement(
+  'h1',
+  {
+    class: 'title',
+  },
+  'Hola Mundo desde create element!'
+);
+
+console.log(element);
+
+class App extends Component {
+  render() {
+    return `
+      <div class="app">
+        <h1>Hola Mundo</h1>
+      </div>
+    `;
+  }
+}
+
+// ${new Wrapper({
+//   children: `
+//     <h1>React.js ⭐⭐⭐⭐⭐</h1>
+//     ${new User({
+//       name: "Ash",
+//       avatar: "./images/ash.jpg",
+//     }).render()}
+//     ${new UserStyled({
+//       name: "Ash",
+//       avatar: "./images/ash.jpg",
+//     }).render()}
+//     `,
+// }).render()}
+
+export default App;
